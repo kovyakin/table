856d0f890575beb0ae4466f74d748643416153a9
kovyakin <kdv-1974@mail.ru>
2024-04-23 22:03:36 +0400

    test 1

--- src/Providers/TableServiceProvider.php
+++ src/Providers/TableServiceProvider.php
@@ -25,7 +25,8 @@ class TableServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/table_vue.php' => config_path('table_vue.php'),
             __DIR__ . '/../resources/views' => resource_path('views/vendor/table'),
-            __DIR__ . '/../public' => public_path('/'),
+            __DIR__ . '/../resources/js' => public_path('/js/'),
+//            __DIR__ . '/../public' => public_path('/'),
         ], 'table_vue');
         Blade::component('package-table', TableComponent::class);
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'table');
